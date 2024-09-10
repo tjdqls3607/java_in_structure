c1c3c0fb6505f492703cd1b14f98e46c600aaf52
정성빈 <tjdqls3124@gmail.com>
2024-09-10 14:50:56 +0900

    패키지와 Sysout을 추기

--- src/Mouse.java
+++ src/Mouse.java
@@ -1,10 +1,17 @@
+package src;
+
 public class Mouse {
     public String name;
     public int age;
     public int countOfTail;
 
-    public void sing() {}
+    public void sing() {
+        System.out.println(name + "찍찍!!!");
+    }
 }
 
-// 패키지 정보도 없고 sing() 메서드 내부에 아무런 로직도 없음.
+/* 1. 패키지 정보도 없고 sing() 메서드 내부에 아무런 로직도 없음.
+   2. 패키지와 Sysout을 추가시켰다.
+ */
+
 
